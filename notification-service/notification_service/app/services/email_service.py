30881558aab55d15abce2394faea99a11af42963
Aashir0920 <114862668+Aashir0920@users.noreply.github.com>
2025-03-03 22:37:49 +0500

    Update email_service.py

--- notification-service/notification_service/app/services/email_service.py
+++ notification-service/notification_service/app/services/email_service.py
@@ -1,1 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+from app.config import config
+
+def send_email(to_email: str, subject: str, message: str):
+    try:
+        msg = MIMEText(message)
+        msg["Subject"] = subject
+        msg["From"] = config.EMAIL_USERNAME
+        msg["To"] = to_email
+
+        with smtplib.SMTP(config.EMAIL_HOST, config.EMAIL_PORT) as server:
+            server.starttls()
+            server.login(config.EMAIL_USERNAME, config.EMAIL_PASSWORD)
+            server.sendmail(config.EMAIL_USERNAME, to_email, msg.as_string())
+
+        print(f"Email sent to {to_email}")
+
+    except Exception as e:
+        print(f"Error sending email: {e}")
  
