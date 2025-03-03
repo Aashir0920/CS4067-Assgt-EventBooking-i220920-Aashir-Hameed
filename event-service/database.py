9b238811fe78d3e31d8cec0f75596805a3e5ae28
Aashir0920 <114862668+Aashir0920@users.noreply.github.com>
2025-03-03 20:29:04 +0500

    Update database.py

--- event-service/database.py
+++ event-service/database.py
@@ -1,1 +1,14 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DATABASE_NAME = "eventdb"
+
+client = MongoClient(MONGO_URI)
+db = client[DATABASE_NAME]
+events_collection = db["events"]
  
