009ad32657f5b746fddd97d9e724267c649df911
Aashir0920 <114862668+Aashir0920@users.noreply.github.com>
2025-03-03 19:08:55 +0500

    Update database.py

--- user-service/database.py
+++ user-service/database.py
@@ -7,8 +7,10 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@localhost:5432/userdb")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Setup Database Connection
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
