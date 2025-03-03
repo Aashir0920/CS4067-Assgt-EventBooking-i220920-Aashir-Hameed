2e51f549fb0bf8206373c2513bf3a0ab979dddfa
Aashir0920 <114862668+Aashir0920@users.noreply.github.com>
2025-03-03 19:10:31 +0500

    Update main.py

--- user-service/main.py
+++ user-service/main.py
@@ -1,1 +1,18 @@
- 
+from fastapi import FastAPI
+from .database import engine, Base
+from .routes import router
+
+# Initialize FastAPI App
+app = FastAPI()
+
+# Create Tables
+Base.metadata.create_all(bind=engine)
+
+# Include Routes
+app.include_router(router, prefix="/users")
+
+# Root Endpoint
+@app.get("/")
+def home():
+    return {"message": "User Service is Running"}
+
