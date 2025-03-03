fa4a5082c1904945fd6d0e2b0d73272952744be3
Aashir0920 <114862668+Aashir0920@users.noreply.github.com>
2025-03-03 20:29:28 +0500

    Update models.py

--- event-service/models.py
+++ event-service/models.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Event(BaseModel):
+    title: str
+    description: str
+    location: str
+    date: str
+    price: float
+    available_tickets: int
+
+class EventUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    location: Optional[str] = None
+    date: Optional[str] = None
+    price: Optional[float] = None
+    available_tickets: Optional[int] = None
  
