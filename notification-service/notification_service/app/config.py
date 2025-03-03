c5bab79a329c63a343e26a1a8ad087e943c72e84
Aashir0920 <114862668+Aashir0920@users.noreply.github.com>
2025-03-03 22:36:03 +0500

    Update config.py

--- notification-service/notification_service/app/config.py
+++ notification-service/notification_service/app/config.py
@@ -1,1 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/")
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.example.com")
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+    EMAIL_USERNAME = os.getenv("EMAIL_USERNAME", "your-email@example.com")
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "your-email-password")
+
+config = Config()
  
